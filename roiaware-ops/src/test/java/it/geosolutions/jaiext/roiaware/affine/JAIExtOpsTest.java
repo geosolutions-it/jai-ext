a61cda90f1ed46e263849349d4934ee8fcfa95f9
Daniele Romagnoli <daniele.romagnoli@geo-solutions.it>
2013-07-04 16:05:07 +0200

    minor fixes on pom

--- roiaware-ops/src/test/java/it/geosolutions/jaiext/roiaware/affine/JAIExtOpsTest.java
+++ roiaware-ops/src/test/java/it/geosolutions/jaiext/roiaware/affine/JAIExtOpsTest.java
@@ -2,18 +2,17 @@ package it.geosolutions.jaiext.roiaware.affine;
 
 import it.geosolutions.jaiext.roiaware.warp.ROIAwareWarpDescriptor;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class JAIExtOpsTest {
-	
-@Test
-public void testRegistration() {
-	
-	// register our own operations
-    ROIAwareWarpDescriptor.register();
-    ScaleDescriptor.register();
-    AffineDescriptor.register();
-    
-    
-	}
+
+    @Test
+    @Ignore
+    public void testRegistration() {
+        // register our own operations
+        ROIAwareWarpDescriptor.register();
+        ScaleDescriptor.register();
+        AffineDescriptor.register();
+    }
 }
